9993d9ed591cd7f2fdff90baf4be55de317835e0
bogdan.feher@gmail.com <bogdan.feher@gmail.com>
2026-01-13 18:27:34 -0500

    added the initial map from google maps

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from "expo-router";
+import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon sf="house.fill" drawable="custom_android_drawable" />
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,4 +1,12 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const INITIAL_REGION = {
+  latitude: 44.28,
+  longitude: -76.51,
+  latitudeDelta: 1,
+  longitudeDelta: 1
+}
 
 export default function Index() {
   return (
@@ -9,7 +17,12 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <MapView 
+        style={StyleSheet.absoluteFill} 
+        provider={PROVIDER_GOOGLE}
+        initialRegion={INITIAL_REGION}
+        showsUserLocation
+        />
     </View>
   );
 }
