d46fa423fab43e054560a140567398ff4d5d2133
Bogdan Feher <bogdan.feher@gmail.com>
2026-01-15 16:26:29 -0500

    fix _layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,11 +1,24 @@
-import "react-native-gesture-handler";
+import { Feather, Ionicons } from '@expo/vector-icons';
+import { router, Stack } from 'expo-router';
+import { Pressable } from 'react-native';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { Slot } from "expo-router";
 
-export default function Layout() {
+export default function TabLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Slot />
+      <Stack>
+        <Stack.Screen name="index" options={{ headerTransparent: true, headerTitle: '', 
+          headerRight: () => 
+          <Pressable onPress={() => router.push('/chat')}>
+            <Ionicons name='chatbox' size={20} style={{ marginLeft: 7 }}/>  
+          </Pressable>,
+          headerLeft: () => 
+          <Pressable onPress={() => router.push('/favourites')}>
+            <Feather name='star' size={20} style={{ marginLeft: 8 }}/>  
+          </Pressable> }} 
+        />
+        <Stack.Screen name="chat" options={{ headerShown: false }} />
+      </Stack>
     </GestureHandlerRootView>
   );
 }
