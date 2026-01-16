32ab083152f627fd778b3bc28f608de3b9f2b1c1
Bogdan Feher <bogdan.feher@gmail.com>
2026-01-16 11:22:03 -0500

    something idk

--- mobile/components/CategoryIcon.tsx
+++ mobile/components/CategoryIcon.tsx
@@ -1,21 +1,57 @@
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
-import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export const CATEGORIES = [
-  { id: "restaurant", name: "Restaurants", icon: "restaurant-outline", provider: "ion" },
-  { id: "bakery", name: "Bakeries", icon: "storefront-outline", provider: "ion" },
-  { id: "charging", name: "Charging Station", icon: "battery-charging-outline", provider: "ion" },
-
-  { id: "shelter", name: "Shelters", icon: "person-shelter", provider: "fa6" }, // FontAwesome6
-  { id: "dropin", name: "Drop-in Centres", icon: "home-outline", provider: "ion" },
-  { id: "meals", name: "Meal Programs", icon: "soup-kitchen", provider: "fa6" }, // FontAwesome6
-  { id: "housing", name: "Housing Services", icon: "information-circle-outline", provider: "ion" },
-
-  { id: "health", name: "Health Services", icon: "medical-services", provider: "material" }, // MaterialIcons
-  { id: "warming", name: "Warm / Cool Sites", icon: "flame-outline", provider: "ion" },
-  { id: "washroom", name: "Washroom / Shower", icon: "shower", provider: "fa6" }, // FontAwesome6
+  {
+    id: "meals",
+    name: "Meal Programs",
+    icon: "soup-kitchen",
+    provider: "fa6",
+  },
+  {
+    id: "washroom",
+    name: "Washrooms & Showers",
+    icon: "shower",
+    provider: "fa6",
+  },
+  {
+    id: "dropin",
+    name: "Drop-in Centres",
+    icon: "home-outline",
+    provider: "ion",
+  },
+  {
+    id: "health",
+    name: "Health Services",
+    icon: "medical-services",
+    provider: "material",
+  },
+  {
+    id: "warming",
+    name: "Warming / Cooling Centres",
+    icon: "flame-outline",
+    provider: "ion",
+  },
+  {
+    id: "shelter",
+    name: "Shelters",
+    icon: "person-shelter",
+    provider: "fa6",
+  },
+  {
+    id: "housing",
+    name: "Housing Services",
+    icon: "home-city-outline",
+    provider: "material",
+  },
+  {
+    id: "other",
+    name: "Other Services",
+    icon: "ellipsis-horizontal-circle-outline",
+    provider: "ion",
+  },
 ];
+
 
 export const CategoryIcon = ({
   provider,
